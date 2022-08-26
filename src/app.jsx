df53facc8efc137dfc907cd3b089f7fdb44bc1a9
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-08-26 20:10:21 -0300

    commit antes da estilização

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './components/common/Navbar'
+
+
+export const App = () => {
+    return (
+        <div className='App'>
+            <Navbar/>
+            <h2>Movies Lib</h2>
+        </div>
+    )
+}
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+
+export const Navbar = () => {
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to="/"><BiCameraMovie /> MoviesLib</Link>
+            </h2>
+            <form action="">
+                <input type="text" placeholder='Busque um Filme' />
+                <button type="submit"><BiSearchAlt2 /></button>
+
+            </form>
+        </nav>
+    )
+}
