b19124f1d7e16664e0198d380c5c6b67d425f2ac
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-09-19 22:45:55 -0300

    Alteraçao no css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -12,34 +12,19 @@ export const Home = () => {
   return (
     <>
       <Navbar />
-
-      <div className='container'>
-        <h2 className='title'>Melhores Filmes:</h2>
-
-        <div className="movies-container">
-
-          {movies.length === 0 && <h1>Nenhum filme encontrado!</h1>}
-
+      <div className="content">
+        <h1 className='title'>Filmes Populares</h1>
+        <ul className='ul-style'>
           {movies.map((movie) => (
-            <div className="card" key={movie.id}>
-
-              <div className='max-h'>
-                <div className="image">
-                  {!movie.poster_path ? <h2>Imagem Indisponivel</h2> : <img src={`${imgPath}${movie.poster_path}`} alt={movie.title} />}
-                </div>
-                <div className="text">
-                  <h3>{movie.title}</h3>
-                </div>
-              </div>
-
+            <li className='li-style' key={movie.id}>
+              {!movie.poster_path ? <h2>Imagem Indisponivel</h2> : <img src={`${imgPath}${movie.poster_path}`} alt={movie.title} />}
+              <span>{movie.title}</span>
               <Link to={`/movie/${movie.id}`}>Detalhes</Link>
-
-            </div>
+            </li>
           ))}
-        </div>
+        </ul>
+
       </div>
-
     </>
-
   )
 }
