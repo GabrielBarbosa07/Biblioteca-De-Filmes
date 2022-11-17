0dd49491c89c3cfbe5a283d1c4301fed073b994e
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-11-17 14:22:35 -0300

    Ajustes nos detalhes

--- src/components/pages/Detalhes.jsx
+++ src/components/pages/Detalhes.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Loading from "../common/Loading/LoadingSpinner"
-import { BiCameraMovie } from 'react-icons/bi'
 import { Link, useParams } from 'react-router-dom'
+import Header from '../common/Header'
+import { Card } from 'react-bootstrap'
 
 const { REACT_APP_API_KEY } = process.env
 
@@ -21,33 +22,26 @@ export default function Detalhes() {
 
     return (
         <main className="main-details">
-            <nav id="navbar">
-                <h2>
-                    <Link to="/"><BiCameraMovie /> MoviesLib</Link>
-                </h2>
-            </nav>
+            <Header />
 
-            <div className='details'>
-                {movie.length === 0 && <Loading />}
+            {movie.length === 0 && <Loading />}
 
-                <div className="card-details" key={movie.id}>
+            <section id="CardContainer">
+                <Card className="bg-black" >
+                    {!movie.poster_path ?
+                        <Card.Img className='max-w' variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt={movie.title} /> :
+                        <Card.Img className="max-w" variant="top" src={`${imgPath}${movie.poster_path}`} alt={movie.title} />}
 
-                    <div className="image">
-                        {!movie.poster_path ? <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png" alt={movie.title} /> : <img src={`${imgPath}${movie.poster_path}`} alt={movie.title} />}
-                    </div>
-
-                    <div className="text">
-                        <div className="description">
-                            <h2>{movie.title}</h2>
-                            <p>{movie.overview === "" ? "Descrição Indisponível!" : movie.overview}</p>
-                        </div>
-                        <p className='release'>{movie.release_date}
-                        </p>
-                        <button><Link to="/">Voltar</Link></button>
-                    </div>
-
-                </div>
-            </ div>
+                    <Card.Body>
+                        <Card.Text>
+                            <h2 className='text-warning'>{movie.title}</h2>
+                            <p className='text-secondary text-align-justify'>{movie.overview === "" ? "Descrição Indisponível!" : movie.overview}</p>
+                            <p className='release text-light'>{movie.release_date}</p>
+                            <Link to="/" className='bg-warning py-2 px-4 text-dark text-decoration-none fw-bold rounded-2'>Voltar</Link>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </section>
         </main>
     )
 }
